14a4eaac93769152a69917c0c755746b9885d483
jgm <jgm@uvm.edu>
2015-12-28 15:45:17 -0500

    Hide-COMUsers.ps1 - Added ability to reconcile currenly hidden mailboxes and contact records with the data feed file.  Added comment-based help.  Added mail output option.  Reformatted output.  Get-ComForwarders.ps1 - Initial attempt at collecting mailbox users who currently forward to the COM mail system.  Currently script execution is /way/ too slow.

---
+++ GET-COMForwarders.ps1
@@ -0,0 +1,45 @@
+﻿param(
+    [Parameter(Mandatory=$false)]
+    [string]$log = 'c:\local\temp\Hide-COMUsers.log'
+)
+Set-PSDebug -Strict
+
+function writeHostAndLog{
+    param(
+    [Parameter(Mandatory=$true)]
+      [string]$Out,
+    [Parameter(Mandatory=$false)]
+      [ValidateSet('Cyan','Yellow','Red','Gray')]
+      [string]$Color
+    )
+    # Writes parameter "Out" to the console and to the global variable $log.
+    #   Optionally will use the specified color for console output.
+    #   Tee-Object largely replaces the need for this function, but does not support color.
+	$out | Out-File -Append -FilePath $log ;
+    if ($color) {
+        Write-Host $out -ForegroundColor $color
+    } else {
+	    Write-Host $out
+    } 
+}
+
+$startTime = Get-Date
+writeHostAndLog -out "Script Started: $startTime" -Color Cyan
+
+if (test-path -LiteralPath $log) {Remove-Item -LiteralPath $log -Force -Confirm:$false}
+
+[string[]]$medUsers = @()
+Get-Mailbox -RecipientTypeDetails UserMailbox -ResultSize Unlimited | 
+    % { 
+        [string]$sam = $_.samAccountName;
+        Get-InboxRule -mailbox $_.DistinguishedName -ea SilentlyContinue -wa SilentlyContinue | 
+            ? {$_.RedirectTo -like "*@med.uvm.edu*"} | % {
+                writeHostAndLog -out "Found $sam" -Color Cyan 
+                $medUsers += $sam
+            }
+    }
+
+writeHostAndLog -out "Script ended: $endTime"
+writeHostAndLog -out " "
+$elapsed = $endTime - $startTime
+writeHostAndLog -out ("Elapsed Time: " + $elapsed.TotalSeconds + " Seconds")
--- Hide-COMUsers.ps1
+++ Hide-COMUsers.ps1
@@ -1,34 +1,63 @@
 ﻿<#
-    Hide-COMUsers.ps1:
-    Takes a list of users from a flat file (extracted from the penguid cluster)
-     - Adds a contact object for all users in the feed.
-     - Hides users in the feed from the GAL.
+.SYNOPSIS
+Modifys records of College of Medicine users by hiding them in the Global Address List and publish an alternate contact record.
+
+.DESCRIPTION
+Takes a list of users from a CSV in the format NetID,emailAddress:
+    - Hides users in the feed from the GAL.
+    - Adds a contact object for all users in the feed.
+Creates a list of all mailboxes hidden in the GAL:
+    - Unhides the mailboxes that are not in in the feed.
+Creates a list of all contact records in the contacts OU:
+    - Removes the contact records that are not in the feed.
+Returns:
+   - 0   - Script ran successfully.
+   - 100 - Could not initialize the PowerShell environment.
+   - 110 - Failed to import $the specified input as CSV.
+   - 200 - Failed to get a list of GAL-hidden mailboxes.
+   - 210 - Failed to get a list of current Mail Contacts Objects.
+
+.PARAMETER file
+Name/Path of a CSV file in NetID,emailAddress format that lists the users to be hidden.
+
+.PARAMETER log
+Name/Path of a file to which to log the actions of this script.  Default value is:
+C:\local\temp\Hide-COMUsers.log
+
+.PARAMETER mail
+Switch value that indicates if the results of the script should be failed.  Default value is $true.
+
 #>
 param(
     [Parameter(Mandatory=$true)]
-    [ValidateScript({Test-Path $_ -PathType 'Leaf'})]
-    [string]$file,
+      [ValidateScript({Test-Path $_ -PathType 'Leaf'})]
+      [string]$file,
     [Parameter(Mandatory=$false)]
-    [string]$log = 'c:\local\temp\Hide-COMUsers.log'
+      [string]$log = 'c:\local\temp\Hide-COMUsers.log',
+    [Parameter(Mandatory=$false)]
+      [Boolean]$mail = $true
 )
 Set-PSDebug -Strict
+
+# Define script-level variables:
+[string]$to = "jgm@uvm.edu"
+[string]$from = "Hide-ComUsers Scheduled Task <Hide-ComUsers@msx-mgt2.campus.ad.uvm.edu>"
+[string]$ContactOU = "OU=Contacts,dc=campus,dc=ad,dc=uvm,dc=edu"
+[string]$managedOU = "OU=people,DC=campus,DC=ad,DC=uvm,DC=edu" 
 
 if (test-path -LiteralPath $log) {Remove-Item -LiteralPath $log -Force -Confirm:$false}
 
 function writeHostAndLog{
     param(
     [Parameter(Mandatory=$true)]
-    [string]$Out,
-
+      [string]$Out,
     [Parameter(Mandatory=$false)]
-    [ValidateSet('Cyan','Yellow','Red','Gray')]
-    [string]$Color
+      [ValidateSet('Cyan','Yellow','Red','Gray')]
+      [string]$Color
     )
-	# Writes the string vailable provided by the -outString parameter to the full log path defined in
-	#	"$log".  This log path needs to be defined globally.
-	# Also echos the string text to the host console.
-	# used in place of "tee-object" cmdlet, which does not have an "-append" parameter.
-	# Returns:  Nothing documented or tested... intended as a blind write.
+    # Writes parameter "Out" to the console and to the global variable $log.
+    #   Optionally will use the specified color for console output.
+    #   Tee-Object largely replaces the need for this function, but does not support color.
 	$out | Out-File -Append -FilePath $log ;
     if ($color) {
         Write-Host $out -ForegroundColor $color
@@ -37,8 +66,23 @@ function writeHostAndLog{
     } 
 }
 
+function showElapsedTime {
+    param(
+        [Parameter(Mandatory=$true)]
+        [datetime]$startTime
+    )
+    writeHostAndLog -out "Script Started: $startTime"
+    $currentTime = get-date
+    writeHostAndLog -out "Current time: $currentTime"
+    writeHostAndLog -out " "
+    $elapsed = $currentTime - $startTime
+    writeHostAndLog -out ("Elapsed Time: " + $elapsed.Hours + ":" + $elapsed.Minutes + ":" + $elapsed.Seconds)
+}
+
 function outMail {
 param (
+    [string]$to,
+    [string]$from,
 	[string]$Subj, 
 	[string]$Body
 	)
@@ -46,12 +90,10 @@ param (
 	# Routing server and to/from addresses can be changed by editing the variables in this function.
 	# Subject and Body must be provided to this function in the form of string variables, 
 	#	using the "-Subj" and "-Body" parameters of this function.
-	# Returns: Nothing.  This is a blind send with no deliver confirmation.
+	# Returns: Nothing.  This is a blind send with no delivery confirmation.
 	$SmtpClient = new-object system.net.mail.smtpClient
-	$SmtpServer = "smtp.uvm.edu"
-	$SmtpClient.host = $SmtpServer
-	$From = "VI Script on vCenter1 <VIScript@vcenter1.campus.ad.uvm.edu>"
-	$To = "saa-vmware@uvm.edu"
+	[string]$SmtpServer = "smtp.uvm.edu"
+	[string]$SmtpClient.host = $SmtpServer
 	$mailMessage = New-Object system.Net.mail.MailMessage($From,$To,$Subj,$Body)
 	$mailMessage.bodyEncoding = [System.Text.Encoding]::UTF8
 #	$mailAttachment = new-Object System.Net.Mail.Attachment($attach)
@@ -59,8 +101,7 @@ param (
 	$SmtpClient.Send($mailMessage) 
 }
 
-
-[string]$ContactOU = "OU=Contacts,dc=campus,dc=ad,dc=uvm,dc=edu"
+#Start logging:
 $startTime = Get-Date
 writeHostAndLog -out "Script Started: $startTime" -Color Cyan
 
@@ -73,23 +114,19 @@ try {
     return 100
 }
 
-<#
-#This block should gather a list of users that currently forward to the @med.uvm.ed mail domain...
-# Could be useful for later revisions of this script.
-[string[]]$medUsers = @()
-Get-Mailbox -RecipientTypeDetails UserMailbox -ResultSize Unlimited | 
-    % { 
-        [string]$sam = $_.samAccountName;
-        Get-InboxRule -mailbox $_.DistinguishedName -ea SilentlyContinue -wa SilentlyContinue | 
-            ? {$_.RedirectTo -like "*@med.uvm.edu*"} | % {
-                writeHostAndLog -out "Found $sam" -Color Cyan 
-                $medUsers += $sam
-            }
-    }
-#>
-
+# Get list of users to hide/contact-enable:
 $users = @()
-$users = Import-Csv -Path $file -header 'name','email' -ea Stop
+try {
+    $users = Import-Csv -Path $file -header 'name','email' -ea Stop
+} catch {
+    writeHostAndLog -Out "Failed to import $file as CSV" -Color Red
+    return 110
+}
+
+###############################################################################
+# Start Hide-in-GAL / Create Contact Object loop:
+#
+writeHostAndLog -out "Hiding from GAL and creating contact objects for users in the feed:" -Color Cyan
 
 $noADAccount   = @()
 $noMailbox     = @()
@@ -99,7 +136,8 @@ $contactFailed = @()
 $contactExists = @()
 
 foreach ($user in $users) {
-    writeHostAndLog -out ("  Processing user: " + $user.name) -Color Cyan
+    #Uncomment the following for verbose reporting on the user currently being processed:
+    #writeHostAndLog -out ("  Processing user: " + $user.name) -Color Cyan
     try {
         $aduser = Get-ADUser -Identity $user.name -Properties `
             Company,Department,DisplayName,Fax,GivenName,Initials,Office,OfficePhone,Surname `
@@ -121,6 +159,7 @@ foreach ($user in $users) {
         continue
     }
     if (-not $mb.HiddenFromAddressListsEnabled) {
+        writeHostAndLog -out ("    Hiding " + $user.name + " from the GAL.") -Color Gray
         try {   
             $mb | Set-Mailbox -HiddenFromAddressListsEnabled $True -ea Stop
         } catch {
@@ -129,10 +168,11 @@ foreach ($user in $users) {
             continue
         }
     } else {
-        writeHostAndLog -out ("    User: " + $user.name + " Already was hidden in the GAL.") -Color Gray
+        # writeHostAndLog -out ("    User: " + $user.name + " Already was hidden in the GAL.") -Color Gray
     }
     # Create a new AD contact object:
     if (-not (Get-Contact -Identity $user.email -ea SilentlyContinue)) {
+        writeHostAndLog -out ("    Creating a contact record for: " + $user.name) -Color Gray
         try {
             #Add phone, department, title
             $mc = New-MailContact -Name $aduser.DisplayName -Alias ($user.name + '-med') `
@@ -158,10 +198,83 @@ foreach ($user in $users) {
         }
     } else {
         #Mail contact object already exists in AD.
-        writeHostAndLog -out ("    Contact object already exists") -color Gray
+        #writeHostAndLog -out ("    Contact object already exists") -color Gray
         $contactExists += $user.name
     }
 }
+showElapsedTime -startTime $startTime
+writeHostAndLog -out " "
+#
+# End Hide-in-GAL / Create Contact Object loop:
+###############################################################################
+
+###############################################################################
+# Start Unhide-in-GAL / Remove Contact Object loop:
+#
+writeHostAndLog -out "Un-hiding mailboxes that are not in the feed:" -Color Cyan
+
+$hiddenUsers  = @()
+$unhideFailed = @()
+
+try {
+    # Find the account names for all Exchange users who are hidden in the GAL:
+    $hiddenUsers = Get-Mailbox -OrganizationalUnit $managedOU -ResultSize Unlimited `
+        -Filter {(HiddenFromAddressListsEnabled -eq $true)} -ea Stop |
+        select -ExpandProperty SamAccountName
+} catch {
+    writeHostAndLog -out "Failed to get a list of GAL-hidden mailboxes." -color Red
+    return 200
+}
+# Loop though all hidden users:
+foreach ($huser in $hiddenUsers) {
+    # Check to see if the current hidden user was in our import list:
+    if (-not $users.name.Contains($huser)) {
+        # Unhide the user
+        try {
+            writeHostAndLog -out ("    Unhiding user: "+ $huser) -Color Gray
+            Set-Mailbox -Identity $huser -HiddenFromAddressListsEnabled $False -ea Stop
+        } catch {
+            writeHostAndLog -out ("    Failed to unhide user: " + $huser) -color Red
+            $unhideFailed += $huser
+            continue
+        }
+    }
+}
+showElapsedTime -startTime $startTime
+writeHostAndLog -out " "
+
+writeHostAndLog -out "Removing contacts that are not in the feed:" -Color Cyan
+$allContacts     = @()
+$rmContactFailed = @()
+try {
+    # Find the Aliases for all existing contact objects:
+    $allContacts = Get-MailContact -OrganizationalUnit $ContactOU -ResultSize Unlimited -ea Stop |
+        select -ExpandProperty Alias
+} catch {
+    writeHostAndLog -out "Failed to get a list of current Mail Contacts Objects." -color Red
+    return 210
+}
+# Loop though all contact objects:
+foreach ($contact in $allContacts) {
+    #Check to see if the current contact is in the import list:
+    if (-not $users.name.Contains($contact.replace('-med',''))) {
+        # Remove the contact
+        try {
+            writeHostAndLog -out ("    Removing contact: " + $contact) -Color Gray
+            Remove-MailContact -Identity $contact -Confirm:$false -ea Stop
+        } catch {
+            writeHostAndLog -out ("    Failed to remove contact for: " + $contact) -color Red
+            $rmContactFailed += $contact
+            continue
+        }
+    }
+}
+#
+# End Unhide-in-GAL / Remove Contact Object loop:
+###############################################################################
+showElapsedTime -startTime $startTime
+writeHostAndLog -out " "
+
 
 writeHostAndLog -out ("List of users with no AD account: ") 
 writeHostAndLog -out ('  ' + $noADAccount) -color Yellow
@@ -171,12 +284,22 @@ writeHostAndLog -out ("List of users who could not be hidden in the GAL: ")
 writeHostAndLog -out ('  ' + $hideFailed) -color Yellow
 writeHostAndLog -out ("List of users for whom contact resource creation failed: ")
 writeHostAndLog -out ('  ' + $contactFailed) -color Yellow
+writeHostAndLog -out ("List of users who could not be un-hidden from the GAL: ")
+writeHostAndLog -out ('  ' + $unhideFailed) -color Yellow
+writeHostAndLog -out ("List of users for whom contact removal failed: ")
+writeHostAndLog -out ('  ' + $rmContactFailed) -color Yellow
+# Verbose information... commented out
 # writeHostAndLog -out ("List of users with addresses already hidden: " + $alreadyHidden) -color Gray
 # writeHostAndLog -out ("List of users who already have a contact: " + $contactExists) -color Gray
 writeHostAndLog -out " "
-writeHostAndLog -out "Script Started: $startTime"
-$endTime = get-date
-writeHostAndLog -out "Script ended: $endTime"
-writeHostAndLog -out " "
-$elapsed = $endTime - $startTime
-writeHostAndLog -out ("Elapsed Time: " + $elapsed.TotalSeconds + " Seconds")
+
+showElapsedTime -startTime $startTime
+
+if ($mail) {
+    [string]$subj = "Hide-ComUsers Scheduled Task for: " + [string]([datetime]::now)
+    [string[]]$bodyArray = Get-Content -Path $log
+    [string]$body = ''
+    foreach ($line in $bodyArray) {$body += $line + "`r`n"}
+    outMail -to $to -from $from -Subj $subj -Body $body
+}
+Return 0
